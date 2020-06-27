e20d3a2e009dac96e789c50fbb07448753854432
giridhar reddy <manasavengalam@gmail.com>
2020-06-27 18:30:59 -0400

    2d commit for the hungry.py file

--- hungry.py
+++ hungry.py
@@ -2,4 +2,24 @@ Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AM
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print('hello i am hungry')
 hello i am hungry
+>>> hungry = input("are you hungry")
+are you hungry
+>>> if hungry=="yes":
+	print("eat food")
+else:
+	print("dont eat food")
+
+	
+dont eat food
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
 >>> 
